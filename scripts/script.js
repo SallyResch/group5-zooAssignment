c99823141e93443c4e68dec29267236eedc9731d
SallyResch <sallyresch27@gmail.com>
2025-11-18 15:43:06 +0100

    added a close button from font awesome and tryed to add javascript to it

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const sidebarDiv = document.querySelector(".sidebar");
 const openBtn = document.querySelector(".sidebar-menu-icon");
 const mainContentDiv = document.querySelector(".main-content");
+const closeButton = document.querySelector(".close-button");
 const initialMainContent = mainContentDiv.innerHTML;
 
 function Animal(name, image, description, food, group, link) {
@@ -102,6 +103,17 @@ video.classList.add("bg_video");
 
 banner.appendChild(video);
 
-openBtn.addEventListener("click", () => {
-  sidebarDiv.classList.toggle("show");
-});
+const openSidebar = () => {
+  openBtn.addEventListener("click", () => {
+    sidebarDiv.classList.toggle("show");
+  });
+}
+openSidebar();
+
+const closeSidebar = () => {
+  closeButton.addEventListener("click", () => {
+    close.classList.toggle("close");
+  })
+}
+
+
