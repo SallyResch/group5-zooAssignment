c50b10faf1f686ecac9d95cf11f04c2ee51eb8fd
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2025-11-18 21:01:02 +0100

    update:click again logic show main_content

--- scripts/mammals.js
+++ scripts/mammals.js
@@ -8,7 +8,9 @@ h2.classList.add("h2");
 sidebar.appendChild(h2);
 
 function Mammal(img, name, aboutShort, aboutFull) {
-  (this.img = img), (this.name = name), (this.aboutShort = aboutShort);
+  this.img = img;
+  this.name = name;
+  this.aboutShort = aboutShort;
   this.aboutFull = aboutFull;
 }
 
@@ -18,12 +20,14 @@ const Echidna = new Mammal(
   "The echidna is a unique egg-laying mammal found in Australia.",
   "The echidna is a unique egg-laying mammal found in Australia. It has spines, a long sticky tongue and feeds mainly on ants and termites."
 );
+
 const Quokka = new Mammal(
   "images/quokka1.jpg",
   "Quokka",
   "The quokka is a small, friendly marsupial native to Western Australia",
   "The quokka is a small, friendly marsupial native to Western Australia. It is known for its adorable smile, round body, and curious nature. Quokkas live mainly on Rottnest Island and feed on leaves, grasses, and shrubs. They are mostly active at night and are not afraid of humans, which makes them very popular among tourists."
 );
+
 const TasmanianDevil = new Mammal(
   "images/tasmanian-devil1.jpg",
   "Tasmanian Devil",
@@ -33,69 +37,45 @@ const TasmanianDevil = new Mammal(
 
 const mammal_arr = [Echidna, Quokka, TasmanianDevil];
 
-let currentAnimal='';
-let click = 0;
-//let originalHTML = main_content.innerHTML;
+let currentAnimal = null;
+
 const display = document.createElement("div");
-display.classList.add('display_content')
+display.classList.add("hide");
+display.classList.add("display");
+
+main_content.parentNode.insertBefore(display, main_content.nextSibling);
+
+const updateDisplay = () => {
+  while (display.firstChild) {
+    display.removeChild(display.firstChild);
+  }
+};
 
 mammal_arr.forEach((mammal) => {
   const el = document.createElement("div");
   el.textContent = mammal.name;
   el.classList.add("el");
+
   el.addEventListener("click", () => {
-
-    // currentAnimal= `${mammal.name} clicked!`;
-    // console.log(currentAnimal); 
-    // click++;
-    // console.log `${click}`
-
-    if(currentAnimal === mammal.name){
-
-      main_content.classList.add('hide');
-      display.classList.add('show')
+    if (!currentAnimal) {
+      currentAnimal = mammal.name;
+      main_content.classList.add("hide");
+      display.classList.remove("hide");
+      updateDisplay();
+    } else if (currentAnimal !== mammal.name) {
+      currentAnimal = mammal.name;
+      updateDisplay();
+    } else {
+      currentAnimal = null;
+      display.classList.add("hide");
+      main_content.classList.remove("hide");
       return;
-
-      // main_content.innerHTML=originalHTML;
-      // display.innerHTML='';
-      // click=0;
-      // currentAnimal=0;
-      // //mammalsAbout.classList.remove('hide');
-      // display.classList.add('show');
-      // currentAnimal= null;
-      //return;
     }
-
-    
-
-    
-
-    
-    //display.classList.add("main_content");
-
-
-
-
-    
-    
-    
-
-   //main_content.classList.add('hide');
-  //   click = 1;
-  // currentAnimal = mammal.name;
-
-  
-  // main_content.innerHTML = "";
-  // display.innerHTML = "";
-
-  //   main_content.innerHTML = "";
-    //main_content.appendChild(display);
 
     const img = document.createElement("img");
     img.src = mammal.img;
     img.classList.add("img");
     display.appendChild(img);
-    main_content.appendChild(display);
 
     const name = document.createElement("h2");
     name.textContent = mammal.name;
@@ -120,15 +100,13 @@ mammal_arr.forEach((mammal) => {
     display.appendChild(infoFull);
     display.appendChild(buttonLess);
 
-    const buttons = [buttonMore, buttonLess];
-
-    buttons.forEach((button) => {
+    [buttonMore, buttonLess].forEach((button) => {
       button.addEventListener("click", () => {
         if (button === buttonMore) {
           buttonMore.classList.add("hide");
           infoFull.classList.remove("hide");
           buttonLess.classList.remove("hide");
-        } else if (button === buttonLess) {
+        } else {
           buttonLess.classList.add("hide");
           infoFull.classList.add("hide");
           buttonMore.classList.remove("hide");
